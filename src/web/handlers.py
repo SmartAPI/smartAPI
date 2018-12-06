0e3594e485344d0a8ad7af78b0250a6aa4185bc3
Marco <artofmarco@gmail.com>
2018-12-06 13:48:09 -0800

    privacy-cookie policy added

--- src/web/handlers.py
+++ src/web/handlers.py
@@ -251,6 +251,12 @@ class AboutHandler(BaseHandler):
         about_output = about_template.render()
         self.write(about_output)
 
+class PrivacyHandler(BaseHandler):
+    def get(self):
+        doc_file = "privacy.html"
+        privacy_template = templateEnv.get_template(doc_file)
+        privacy_output = privacy_template.render()
+        self.write(privacy_output)
 
 APP_LIST = [
     (r"/", MainHandler),
@@ -269,5 +275,6 @@ APP_LIST = [
     (r"/guide/?", GuideHandler),
     (r"/editor/(.+)/?", APIEditorHandler),
     (r"/editor/?", APIEditorHandler),
-    (r"/about/?", AboutHandler)
+    (r"/about/?", AboutHandler),
+    (r"/privacy/?", PrivacyHandler)
 ]
