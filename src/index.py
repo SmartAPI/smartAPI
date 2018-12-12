dc2f73f29a5e07ef355d1295ccf4e38fa0edb011
Jerry <xzhou@scripps.edu>
2018-12-12 12:36:22 -0800

    Cleaned working directory

--- src/index.py
+++ src/index.py
@@ -1,39 +1,10 @@
-import sys
-import os.path
-import json
-
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-import tornado.escape
-from tornado.options import define, options
+from biothings.web.index_base import main, options
 
 from api.handlers import APP_LIST as api_app_list
 from web.handlers import APP_LIST as web_app_list
+
+import os.path
 import config
-
-src_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
-if src_path not in sys.path:
-    sys.path.append(src_path)
-print(src_path)
-
-STATIC_PATH = os.path.join(src_path, 'src/static')
-
-define("port", default=8000, help="run on the given port", type=int)
-define("address", default="127.0.0.1", help="run on localhost")
-define("debug", default=False, type=bool, help="run in debug mode")
-tornado.options.parse_command_line()
-if options.debug:
-    import tornado.autoreload  # pylint: disable=C0412
-    import logging
-    logging.getLogger().setLevel(logging.DEBUG)
-    options.address = '0.0.0.0'
-
-
-def get_json(filename):
-    with open(filename) as f:
-        return json.load(f)
 
 
 def add_apps(prefix='', app_list=None):
@@ -60,33 +31,10 @@ APP_LIST = [
 APP_LIST += add_apps('', web_app_list)
 APP_LIST += add_apps('api', api_app_list)
 
-
-settings = {
-    "cookie_secret": config.COOKIE_SECRET
-}
-if options.debug:
-    settings.update({
-        "static_path": STATIC_PATH,
-        "debug": True
-    })
-
-
-def main():
-    # required for proper github oauth authentication
-    # ref: https://github.com/jkeylu/torngithub
-    tornado.httpclient.AsyncHTTPClient.configure(
-        "tornado.curl_httpclient.CurlAsyncHTTPClient")
-
-    application = tornado.web.Application(APP_LIST, **settings)
-    http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(options.port, address=options.address)
-    loop = tornado.ioloop.IOLoop.instance()
-    if options.debug:
-        # tornado.autoreload.start(loop)
-        logging.info('Server is running on "%s:%s"...',
-                     options.address, options.port)
-    loop.start()
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    src_path = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
+    static_path = os.path.join(src_path, 'src', 'static')
+    main(APP_LIST,
+         app_settings={"cookie_secret": config.COOKIE_SECRET},
+         debug_settings={"static_path": static_path},
+         select_curl=True)
