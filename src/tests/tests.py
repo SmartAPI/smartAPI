67ec49fa8805f565fc0063dfb01691d084688203
Jerry <xzhou@scripps.edu>
2019-03-15 17:20:28 -0700

    Support Local Test with Biothings Testing Utility

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,90 +1,28 @@
-import requests
-from nose.tools import eq_, ok_
+'''
+    Data-safe test for SmartAPI
+'''
+
 import json
-import sys
 import os
-import re
-import unittest
+
+from nose.core import run
+from nose.tools import eq_
+from tornado.web import Application
+
+from biothings.tests.test_helper import (BiothingsTestCase,
+                                         TornadoTestServerMixin)
+from biothings.web.settings import BiothingESWebSettings
 
 _d = json.loads    # shorthand for json decode
 _e = json.dumps    # shorthand for json encode
 
 
-class SmartAPITest(unittest.TestCase):
+class SmartAPIRemoteTest(BiothingsTestCase):
     __test__ = True  # explicitly set this to be a test class
-
-    #############################################################
-    # Test functions                                            #
-    #############################################################
 
     host = os.getenv("SMARTAPI_HOST", "https://smart-api.info")
     host = host.rstrip('/')
     api = host + '/api'
-
-    ############################################################
-    # Helper functions                                          #
-    #############################################################
-
-    def truncate(self, s, limit):
-        '''truncate a string.'''
-        if len(s) <= limit:
-            return s
-        else:
-            return s[:limit] + '...'
-
-    def get_ok(self, url):
-        res = requests.get(url)
-        assert res.status_code == 200, "status {} != 200 for GET to url: {}".format(
-            res.status_code, url)
-        return res
-
-    def get_status_code(self, url, status_code):
-        res = requests.get(url)
-        assert res.status_code == status_code, "status {} != {} for GET to url: {}".format(
-            res.status_code, status_code, url)
-        return res
-
-    def post_status_code(self, url, params, status_code):
-        headers = {'Content-type': 'application/x-www-form-urlencoded'}
-        res = requests.post(url, data = params, headers=headers)
-        assert res.status_code == status_code, "status {} != {} for url: {}\nparams: {}".format(res.status_code, status_code, url, params)
-        return res
-
-    def get_404(self, url):
-        self.get_status_code(url, 404)
-
-    def get_405(self, url):
-        self.get_status_code(url, 405)
-
-    def head_ok(self, url):
-        res = requests.head(url)
-        assert res.status_code == 200, "status {} != 200 for HEAD to url: {}".format(res.status_code, url)
-
-    def post_ok(self, url, params):
-        return self.post_status_code(url, params, 200)
-
-    def query_has_hits(self, q, query_endpoint='query'):
-        d = self.json_ok(self.get_ok(
-            self.api + '/' + query_endpoint + '?q=' + q))
-        assert d.get('total', 0) > 0 and len(d.get('hits', [])) > 0
-        return d
-
-    def json_ok(self, s, checkerror=True):
-        d = _d(s.text)
-        if checkerror:
-            assert not (isinstance(d, dict)
-                        and 'error' in d), self.truncate(str(d), 100)
-        return d
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    ############################################################################################
-    # Smart API tests
-    ############################################################################################
 
     def test_query_all_has_hits(self):
         self.query_has_hits('__all__')
@@ -101,11 +39,13 @@ class SmartAPITest(unittest.TestCase):
         eq_(res['hits'], [])
 
     def test_query_string_not_provided(self):
-        self.get_status_code(self.api + '/query', 400)
+        self.get_status_match(self.api + '/query', 400)
 
     def test_query_multiple_filters_biothings(self):
-        res = self.json_ok(self.get_ok(self.api +
-                                       '/query?q=__all__&filters={"tags.name.raw":["annotation","variant"],"info.contact.name.raw":["Chunlei Wu"]}'))
+        res = self.json_ok(
+            self.get_ok(
+                self.api +
+                '/query?q=__all__&filters={"tags.name.raw":["annotation","variant"],"info.contact.name.raw":["Chunlei Wu"]}'))
         eq_(len(res['hits']), 3)
 
     def test_query_specified_fields(self):
@@ -144,19 +84,36 @@ class SmartAPITest(unittest.TestCase):
         eq_(len(res['hits']), 6)
 
     def test_query_invalid_size(self):
-        self.get_status_code(self.api + '/query?q=__all__&size=my', 400)
+        self.get_status_match(self.api + '/query?q=__all__&size=my', 400)
 
-    def test_query_invalid_from(self): 
+    def test_query_invalid_from(self):
         res_0 = self.json_ok(self.get_ok(self.api +
-                                       '/query?q=__all__&fields=_id&size=5'))
+                                         '/query?q=__all__&fields=_id&size=5'))
         ids_0 = set([hit['_id'] for hit in res_0['hits']])
         res_1 = self.json_ok(self.get_ok(self.api +
-                                       '/query?q=__all__&fields=_id&size=5&from=5'))
+                                         '/query?q=__all__&fields=_id&size=5&from=5'))
         ids_1 = [hit['_id'] for hit in res_1['hits']]
         for _id in ids_1:
             if _id in ids_0:
-                self.assertTrue(False)
+                assert False
 
 
-if __name__ == '__main__':
-    unittest.main()
+class SmartAPILocalTest(TornadoTestServerMixin, SmartAPIRemoteTest):
+    '''
+        Starts a Tornado server on its own and perform tests against this server.
+    '''
+    __test__ = True  # explicitly set this to be a test class
+
+    @classmethod
+    def setup_class(cls):
+        ''' Reads Tornado Settings from config.py '''
+        cls.WEB_SETTINGS = BiothingESWebSettings(config='config')
+        cls.APP_LIST = cls.WEB_SETTINGS.generate_app_list()
+        cls.STATIC_PATH = cls.WEB_SETTINGS.STATIC_PATH
+
+    def get_app(self):
+        return Application(self.APP_LIST, static_path=self.STATIC_PATH)
+
+
+if __name__ == "__main__":
+    run(defaultTest='src.tests')
