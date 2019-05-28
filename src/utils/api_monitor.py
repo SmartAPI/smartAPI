cf51fa245ca2c3e754fa4bbca075ec3496f5051e
Jerry <xzhou@scripps.edu>
2019-05-28 12:47:21 -0700

    Annotate classes

--- src/utils/api_monitor.py
+++ src/utils/api_monitor.py
@@ -1,3 +1,11 @@
+'''
+    API Uptime Monitor
+
+        - Status: Good, Bad, Incompatible, Unknown
+
+        - Run this file directly to perform status check
+'''
+
 import logging
 from datetime import datetime
 
@@ -5,8 +13,8 @@ import requests
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 from elasticsearch_dsl.connections import connections
-from requests.packages.urllib3.exceptions import \
-    InsecureRequestWarning  # pylint:disable=import-error
+# pylint:disable=import-error, ungrouped-imports
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from tornado.ioloop import IOLoop
 
 connections.create_connection(hosts=['localhost'])
@@ -25,7 +33,8 @@ class DictQuery(dict):
         for key in keys:
             if val:
                 if isinstance(val, list):
-                    val = [v.get(key, default) if v else None for v in val]
+                    val = [v.get(key, default)
+                           if v else None for v in val]  # pylint:disable=not-an-iterable
                 else:
                     val = val.get(key, default)
             else:
@@ -38,10 +47,14 @@ class DictQuery(dict):
 
 
 class API:
+    '''
+        An API corresponding to an es document
+    '''
+
     def __init__(self, api_doc):
         # default status of API is unknown, since some APIs doesn't provide
         # examples as values for parameters
-        self.id = api_doc['_id']
+        self.id = api_doc['_id']  # pylint: disable=invalid-name
         self.api_status = 'unknown'
         self.name = api_doc['info']['title']
         try:
@@ -59,8 +72,13 @@ class API:
         self.endpoints_info = new_path_info
 
     def check_api_status(self):
-        # loop through each endpoint and extract parameter & example $ HTTP
-        # method information
+        '''
+            loop through each endpoint and extract parameter & example $ HTTP method information
+        '''
+
+        if not self.api_server:
+            return
+
         for _endpoint, _endpoint_info in self.endpoints_info.items():
             endpoint_doc = {'name': '/'.join(s.strip('/') for s in(self.api_server, _endpoint)),
                             'components': self.components}
@@ -73,16 +91,28 @@ class API:
                 endpoint_doc['requestbody'] = _endpoint_info['post'].get('requestBody')
             if endpoint_doc.get('params'):
                 endpoint = Endpoint(endpoint_doc)
-                response = endpoint.make_api_call()
-                if response:
-                    status = endpoint.check_response_status(response)
-                    if status == 200:
-                        self.api_status = 'good'
-                    else:
-                        self.api_status = 'bad'
+                try:
+                    response = endpoint.make_api_call()
+                except Exception as exception:  # pylint: disable=broad-except
+                    logger = logging.getLogger("utils.uptime.API")
+                    logger.error(exception)
+                else:
+                    if response:
+                        status = endpoint.check_response_status(response)
+                        if status == 200:
+                            self.api_status = 'good'
+                        else:
+                            self.api_status = 'bad'
+
+    def __str__(self):
+        return f"{self.id}: {self.api_status} ({self.name})"
 
 
 class Endpoint:
+    '''
+        An API Endpoint
+    '''
+
     def __init__(self, endpoint_doc):
         self.endpoint_name = endpoint_doc['name']
         self.method = endpoint_doc['method']
@@ -92,6 +122,7 @@ class Endpoint:
 
     def make_api_call(self):
         url = self.endpoint_name
+        logger = logging.getLogger("utils.uptime.endpoint.make_api_call")
         # handle API endpoint which use GET HTTP method
         if self.method == 'GET':
             params = {}
@@ -153,7 +184,7 @@ class Endpoint:
                         example = schema.get('example')
                         ref = schema.get('$ref')
                         if example:
-                            logging.debug(url)
+                            logger.debug(url)
                             try:
                                 response = requests.post(url,
                                                          timeout=3,
@@ -163,20 +194,20 @@ class Endpoint:
                             except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
                                 pass
                         elif ref:
-                            logging.debug(url)
+                            logger.debug(url)
                             if ref.startswith('#/components/'):
                                 component_path = ref[13:]
                                 component_path += '/example'
-                                logging.debug('component path: {}'.format(component_path))
+                                logger.debug('component path: %s', component_path)
                                 example = DictQuery(self.components).get(component_path)
-                                logging.debug('example %s', example)
+                                logger.debug('example %s', example)
                                 if example:
                                     try:
                                         response = requests.post(url,
                                                                  timeout=3,
                                                                  json=example,
                                                                  verify=False)
-                                        logging.debug(response)
+                                        logger.debug(response)
                                         return response
                                     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
                                         pass
@@ -195,62 +226,86 @@ class Endpoint:
 
 
 async def update_uptime_status():
-
+    '''
+        Perform Periodic Update to Uptime Status in ES
+    '''
     logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger("utils.uptime.es")
+    logger = logging.getLogger("utils.uptime.es.update")
 
-    def func():
-
-        count = 0
+    def sync_func():
 
         search = Search(index='smartapi_oas3').query("match_all")
-        logger.info("Found %s documents", search.count())
-        for hit in list(search.scan()):
+
+        total = search.count()
+        result = {}
+
+        logger.info("Found %s documents", total)
+
+        for index, hit in enumerate(search.scan()):
+
             doc = hit.to_dict()
             doc['_id'] = hit.meta.id
+
             api = API(doc)
-            if api.api_server:
-                try:
-                    api.check_api_status()
-                except Exception as e:
-                    logger.warning("%s : %s", hit.meta.id, e)
-                    continue
-                logger.info("%s : %s", hit.meta.id, api.api_status)
-                es_params = {
-                    "id": hit.meta.id,
-                    "index": 'smartapi_oas3',
-                    "doc_type": 'api'
-                }
-                partial_doc = {
-                    "doc": {
-                        "_meta": {
-                            "uptime_status": api.api_status,
-                            "uptime_ts": datetime.utcnow()
-                        }
+            api.check_api_status()
+
+            logger.info("[%s/%s] %s", index+1, total, api)
+
+            doc_params = {
+                "id": hit.meta.id,
+                "index": 'smartapi_oas3',
+                "doc_type": 'api'
+            }
+            partial_update = {
+                "doc": {
+                    "_meta": {
+                        "uptime_status": api.api_status,
+                        "uptime_ts": datetime.utcnow()
                     }
                 }
-                es_client = Elasticsearch()
-                res = es_client.update(body=partial_doc, **es_params)
-                logger.debug(res)
-                count += 1
-            else:
-                logger.warning("%s : No API Server.", hit.meta.id)
+            }
+            es_client = Elasticsearch()
+            res = es_client.update(body=partial_update, **doc_params)
 
-        return count
+            logger.debug(res)
+
+            result[api.api_status] = result.get(api.api_status, 0) + 1
+
+        return result
 
     logger.info("Start scheduled uptime check...")
-    count = await IOLoop.current().run_in_executor(None, func)
-    logger.info("Uptime updated for %s documents.", count)
+    result = await IOLoop.current().run_in_executor(None, sync_func)
+    logger.info("Uptime updated. %s", result)
+
+
+def main():
+    '''
+        Check Production Server API Status
+
+            - Output result to screen and file
+
+            - Do not make changes to es index
+    '''
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("utils.uptime.main")
+    logger.info("Start checking API status.")
+
+    # call smartapi API to fetch all API metadata in registry
+    api_docs = requests.get('https://smart-api.info/api/query?q=__all__&size=100').json()
+    logger.info("Retrieved %s api documents.", api_docs['total'])
+
+    # can rename or specify a specific folder to put the file
+    output_file_name = 'smarapi_uptime_robot' + datetime.today().strftime('%Y-%m-%d') + '.txt'
+    with open(output_file_name, 'w') as f:  # pylint: disable=invalid-name
+
+        for api_doc in api_docs['hits']:
+            api = API(api_doc)
+            api.check_api_status()
+            logger.info("%s : %s", api.id, api.api_status)
+            f.write(api.id + '\t' + api.api_status + '\n')
+
+    logger.info("Done.")
 
 
 if __name__ == '__main__':
-    # call smartapi API to fetch all API metadata in registry
-    api_docs = requests.get('https://smart-api.info/api/query?q=__all__&size=100').json()
-    # can rename or specify a specific folder to put the file
-    output_file_name = 'smarapi_uptime_robot' + datetime.today().strftime('%Y-%m-%d') + '.txt'
-    with open(output_file_name, 'w') as f:
-        for api_doc in api_docs['hits']:
-            api = API(api_doc)
-            if api.api_server:
-                api.check_api_status()
-            f.write(api.id + '\t' + api.api_status + '\n')
+    main()
