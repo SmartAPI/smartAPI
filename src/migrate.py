26ce05c0bfdcb2121a909173b852f95b7abdc2af
Jerry <xzhou@scripps.edu>
2021-02-07 17:19:43 -0800

    Revise migration script

--- src/migrate.py
+++ src/migrate.py
@@ -3,71 +3,14 @@ import base64
 from elasticsearch.client import Elasticsearch
 from elasticsearch.helpers import scan
 
-from controller import ControllerError, OpenAPI, SmartAPI, Swagger
-from model import APIDoc
-from utils import decoder
+from controller import SmartAPI
+from utils import decoder, indices
 
 ES_ORIGIN = "http://smart-api.info:9200"
-ES_DESTINATION = "http://18.237.41.215:9200"
+ES_DESTINATION = "http://localhost:9200"  # CANNOT CHANGE THIS
 
 
-class MigrationSmartAPI(SmartAPI):
-
-    def validate(self):
-
-        if not self.version:
-            raise ControllerError("Unknown version.")
-
-        if self.version == 'openapi':
-            doc = OpenAPI(self._data)
-        else:  # then it must be swagger
-            doc = Swagger(self._data)
-
-        # OVERRIDE
-        # try:
-        #     doc.validate()  # basing on its format
-        # except ValueError as err:
-        #     raise ControllerError(str(err)) from err
-
-        return doc
-
-    def save(self, timestamp, **kwargs):
-
-        if not self.username:
-            raise ControllerError("Username is required.")
-
-        if self.url is self.VALIDATION_ONLY:
-            raise ControllerError("In validation-only mode.")
-
-        _doc = self.validate()
-        _doc.clean()  # only keep indexing fields
-
-        # OVERRIDE
-        # if self.slug:
-        #     _id = self.find(self.slug)
-        #     if _id and _id != self._id:  # another doc same slug.
-        #         raise ConflictError("Slug is already registered.")
-
-        # NOTE
-        # if the slug of another document changed at this point
-        # it's possible to have two documents with the same slug
-        # registered. but it should be rare enough in reality.
-
-        doc = APIDoc(**_doc)
-        doc.meta.id = self._id
-        doc._meta.url = self.url
-        doc._meta.timestamp = timestamp  # OVERRIDE
-        doc._meta.username = self.username
-        doc._meta.slug = self.slug
-        doc._raw = decoder.compress(self.raw)
-        doc.save(**kwargs)  # OVERRIDE
-
-        return self._id
-
-
-def main():
-
-    client = Elasticsearch(ES_DESTINATION)
+def migrate():
 
     for doc in scan(
         Elasticsearch(ES_ORIGIN),
@@ -80,11 +23,33 @@ def main():
             url = doc['_source']['_meta']['url']
             raw = decoder.decompress(base64.urlsafe_b64decode(doc['_source']['~raw']))
 
-            smartapi = MigrationSmartAPI(url, raw)
+            smartapi = SmartAPI(url, doc['_source']['_meta']['timestamp'])
+            smartapi.raw = raw
             smartapi.username = doc['_source']['_meta']['github_username']
             smartapi.slug = doc['_source']['_meta'].get('slug')
-            smartapi.save(doc['_source']['_meta']['timestamp'], using=client)
+            smartapi.save(update_ts=False)
+
+
+def update():
+
+    for doc in scan(
+        Elasticsearch(ES_DESTINATION),
+        query={"query": {"match_all": {}}},
+        index="smartapi_docs",
+        scroll="60m"
+    ):
+        print(doc["_id"])
+        smartapi = SmartAPI.get(doc["_id"])
+        print(smartapi.check())
+        print(smartapi.refresh())
+        if smartapi.webdoc.status == 299:
+            smartapi.webdoc._status = 200  # change status not reliable during migration
+        smartapi.save(update_ts=False)
 
 
 if __name__ == "__main__":
-    main()
+    input('Will reset smartapi_docs index. Ctrl-C to cancel.')
+    indices.reset()
+    migrate()
+    indices.refresh()
+    update()
