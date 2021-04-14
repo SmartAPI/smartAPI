f073537406be6020edfd5a5682b2583ff81856eb
Marco Cano <artofmarco@gmail.com>
2021-04-14 12:59:52 -0700

    default meta tags

--- web-app/src/components/MetaHead.vue
+++ web-app/src/components/MetaHead.vue
@@ -1,18 +1,18 @@
 <template>
-  <teleport to="head">
+  <!-- <teleport to="head">
     <title>{{title}}</title>
-    <meta property="og:site_name" :content="title">
     <meta name="description" :content="description">
+    <meta property="og:site_name" :content="title">
+    <meta property="fb:app_id" :content="title"/>
+    <meta property="og:description" :content="description" >
     <meta property="og:url" :content="'https://smart-api.info'+path">
     <meta property="og:title" :content="title"/>
-    <meta name="description" :content="description" />
     <meta name="twitter:description" :content="description" />
     <meta name="twitter:url" :content="'https://smart-api.info'+path"/>
-    <meta property="og:description" :content="description" >
     <meta name="twitter:title" :content="title"/>
-    <meta property="fb:app_id" :content="title"/>
     <meta name="twitter:card" :content="description"/>
-  </teleport>
+  </teleport> -->
+  <div></div>
 </template>
 
 <script>
@@ -32,6 +32,21 @@ export default {
     path:function(){
       return this.$route.fullPath
     }
+  },
+  mounted: function(){
+    //title
+    document.querySelector("title").innerHTML = this.title
+    document.querySelector("meta[name='twitter:title']").setAttribute('content', this.title);
+    document.querySelector("meta[property='og:site_name']").setAttribute('content', this.title);
+    document.querySelector("meta[property='fb:app_id']").setAttribute('content', this.title);
+    document.querySelector("meta[property='og:title']").setAttribute('content', this.title);
+    //desc
+    document.querySelector("meta[name='description']").setAttribute('content', this.description);
+    document.querySelector("meta[name='twitter:description']").setAttribute('content', this.description);
+    document.querySelector("meta[name='twitter:card']").setAttribute('content', this.description);
+    //path
+    document.querySelector("meta[name='twitter:url']").setAttribute('content', 'https://smart-api.info'+this.path);
+    document.querySelector("meta[property='og:url']").setAttribute('content', 'https://smart-api.info'+this.path);
   }
 }
 </script>
