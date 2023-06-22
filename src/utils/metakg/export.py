dd6bbe14b41326d866702996a58ea53888f88838
Nichollette <nichollette.aco@gmail.com>
2023-06-22 16:58:05 -0400

    added comment formatting and logic to export func

--- src/utils/metakg/export.py
+++ src/utils/metakg/export.py
@@ -1,5 +1,7 @@
 import logging
+from warnings import WarningMessage
 import xml.etree.ElementTree as ET
+import textwrap
 
 from controller import SmartAPI
 
@@ -9,67 +11,25 @@ logger = logging.getLogger("metakg_export")
 Export edge data into graphml format
 """
 
-def compare_hits(mkg_id, hits_total, predicate=None):
-    """
-    Get MetaKG API
-        - using built-in smartapi funcitons, get & return a datasets metakg api 
-        Input:
-            mkg_id: string id of the requested api
-            predicate: (optional) string of a requested predicate, default None
-        Returns: 
-            True - if the given API hits total equal the tested hits total, or
-            False - if the given API hits do not equal the tested hits total
-    """
-    api = SmartAPI.get(mkg_id)
-    test_hits = api.get_metakg()
-    print("LENGTH: ", len(test_hits))
-    if predicate:
-        test_hits =[d for d in test_hits if d['predicate'] == predicate]
-    if len(test_hits) == hits_total:
-        logger.info("Full data being retrieved, data is sufficient")
-        return True
-    else:
-        logger.warning('Data hits retrieved do not match, API call retrieved %s, and test retrieval has %s'%(hits_total, len(test_hits)))
-        return False
-
-def write_graphml(tree, filename=None):
-    """
-    Output graphML file
-        Input: 
-            tree: graphml (xml) tree
-            outfile: graphml file name, i.e "mygraph.graphml"
-        Returns:
-            Writes graphml file, else raises error.
-    """
-    if filename:
-        tree.write(filename)
-    else:
-        logger.info('[write_graphml] no filename given')
-
-def edges2graphml(edges, q_id, api_call, edge_default="directed"):
+def edges2graphml(chunk, _id, api_call, edge_default="directed"):
     """
     Build a graphML (xml) tree
     Input:
-        edges: list of dictionaries representing a knowledge graph with subject/object/predicate keys
-        q_id: dataset ID
+        chunk: list of dictionaries representing a knowledge graph with subject/object/predicate keys
+        _id: dataset ID
         api_call: the API path
         edge_default: directed or undirected graph, default='directed'
     Returns: a graphml (xml) string -- automatically downloads file unless, &download=False, flag is added
     Notes:
-        * added information comments to the top of the file
-        * added data hits total to the top of the file
-    - Do we want to make the edge default direction customizable?
+        * summary info, data total size (edge size), and warning comments 
+        were added to the top of the file
     """
-
-    root = ET.Element('graphml')
+    edges = chunk['hits']
+    expected_total = chunk['total']
+    
+    root = ET.Element('graphml') # set root
     root.set('xmlns', 'http://graphml.graphdrawing.org/xmlns')
 
-    # Add comment with data length
-    comment_data = ET.Comment(f"This edge data (ID:{q_id}) has been transformed into GraphML format, with edge default={edge_default}. The full API path called - {api_call}.")
-    comment_data_length = ET.Comment(f"Edge Count: {len(edges)}")
-    root.append(comment_data)
-    root.append(comment_data_length)
-    
     # Define key for node data
     key_node = ET.SubElement(root, 'key')
     key_node.set('id', 'd1')
@@ -104,7 +64,38 @@ def edges2graphml(edges, q_id, api_call, edge_default="directed"):
         data_edge.set('key', 'd2')
         data_edge.text = data['predicate']
 
-    tree = ET.ElementTree(root)
+    # tree = ET.ElementTree(root)
     graphml_string = ET.tostring(root, encoding="utf-8", method="xml").decode()
-    
-    return graphml_string
+
+    # Write Comments to top of file
+    # set the summary text
+    summary_text = f"This edge data (ID:{_id}) has been transformed into GraphML format. Curated from SmartAPIs meta knowledge database, the full API path can be seen here: {api_call}."
+    # set the data length comment text
+    data_length_text = f"Edge Count: {len(edges)}"    
+    # wrap the summary text
+    wrapped_summary_text = textwrap.fill(summary_text, width=100)
+    # place both texts into a comment
+    comment_element = f"<!-- {wrapped_summary_text} -->\n<!-- {data_length_text} -->\n"
+
+    # add a warning comment if the total data hits (chunk['hits]) isn't equal to chunk['total']
+    if expected_total > 5000: 
+        # if the expected total is greater than the size limit, 5000, data has been truncated
+        warning_text =  f'WARNING: total data size has been truncated to size {len(edges)}. The total data size expected, {expected_total}, exceeds the API size limit (&size=5000).'
+        wrapped_warning_text = textwrap.fill(warning_text, width=100)
+        warning_comment_element = f"<!-- {wrapped_warning_text} -->\n"
+        # concat comment with the graphml string 
+        graphml_string_with_comment = comment_element + warning_comment_element + graphml_string
+    elif expected_total != len(edges):
+        # if the expected total isn't equal to the total hits that are returned(edges),
+        # give a warning comment for the user.
+        warning_text = f'WARNING: the size total of the edges(data hits) given, {len(edges)}, does not equal the expected size of the API, {expected_total}.'
+        wrapped_warning_text = textwrap.fill(warning_text, width=100)
+        warning_comment_element = f"<!-- {wrapped_warning_text} -->\n"
+        # concat comment with the graphml string 
+        graphml_string_with_comment = comment_element + warning_comment_element + graphml_string
+    else:
+        # concat comment with the graphml string 
+        graphml_string_with_comment = comment_element + graphml_string
+
+
+    return graphml_string_with_comment
