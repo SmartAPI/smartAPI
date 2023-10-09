3dd080e4cf7b608cc49dd1383d2eef7cd381cc9f
Nichollette <nichollette.aco@gmail.com>
2023-10-09 16:00:22 -0400

    added path finder test file

---
+++ src/tests/_utils/metakg/integration/parser/path_finder.py
@@ -0,0 +1,52 @@
+import unittest
+import networkx.classes.digraph as nx_digraph
+
+from utils.metakg.path_finder import MetaKGPathFinder
+
+class TestPathFinderClass(unittest.TestCase):
+
+    def test_get_graph(self):
+        query_data = {
+            'q': 'api.name:BTE'
+        }
+
+        pf = path_finder.MetaKGPathFinder(query_data)
+        G = pf.G
+
+        self.assertInstance(G, nx_digraph.DiGraph) 
+
+    def test_get_paths(self):
+        subject="ComplexMolecularMixture"
+        object="DiseaseOrPhenotypicFeature"
+        cutoff=3
+        query_data = {
+            'q': 'api.name:BTE'
+        }
+
+        pf = path_finder.MetaKGPathFinder(query_data)
+        paths = pf.get_paths(subject=subject, object=object, cutoff=cutoff)
+        self.assertInstance(paths, list)
+        for data in paths[:2]:
+            self.assertIn(data, ['path', 'edges'])
+            self.assertIsInstance(data['path'], list)
+            for edge in data['edges']:
+                self.assertIn(data['edges'], 'subject')
+                self.assertIn(data['edges'], 'object')
+
+
+        # Test Cutoff values
+        # cutoff_values = [ 3, 4, 5 ] 
+
+        # for cutoff in cutoff_values:
+        #     start_time = time.time()
+        #     paths = pf.get_paths(subject=subject, object=object, cutoff=cutoff)
+        #     end_time = time.time()
+
+        #     time_taken[cutoff] = {
+        #         'duration': end_time - start_time,
+        #         'path_ct': len(paths)
+        #     }
+
+
+    # SELF CYCLE TEST: &object=DiseaseOrPhenotypicFeature&subject=DiseaseOrPhenotypicFeature
+    # SELF CYCLE TEST: &object=DiseaseOrPhenotypicFeature&subject=DiseaseOrPhenotypicFeature
