90f4676067ddc56b27a2ce33aefdc096b7ffe821
Nichollette <nichollette.aco@gmail.com>
2024-06-12 17:10:08 -0400

    updated tests for validation schemas

--- src/tests/validate/test_validate.py
+++ src/tests/validate/test_validate.py
@@ -9,6 +9,12 @@ dirname = os.path.dirname(__file__)
 with open(os.path.join(dirname, "openapi-pass.json"), "rb") as file:
     PASS_OPENAPI = file.read()
 
+with open(os.path.join(dirname, "openapi3.1-pass.json"), "rb") as file:
+    PASS_OPENAPI31 = file.read()
+
+with open(os.path.join(dirname, "openapi3.1-fail.json"), "rb") as file:
+    FAIL_OPENAPI31 = file.read()
+
 with open(os.path.join(dirname, "swagger-pass.json"), "rb") as file:
     PASS_SWAGGER = file.read()
 
@@ -23,24 +29,24 @@ with open(os.path.join(dirname, "x-translator-fail-2.yml"), "rb") as file:
 
 
 def test_01():
-    validate(PASS_OPENAPI, {"openapi": openapis["openapi_v3"]})
+    validate(PASS_OPENAPI, {"openapi": openapis["openapi_v3.0"]})
     validate(PASS_OPENAPI, openapis)
 
 
 def test_02():
-    validate(PASS_TRANSLATOR, {"openapi": openapis["openapi_v3"]})
+    validate(PASS_TRANSLATOR, {"openapi": openapis["openapi_v3.0"]})
     validate(PASS_TRANSLATOR, {"x-translator": openapis["x-translator"]})
     validate(PASS_TRANSLATOR, openapis)
 
 
 def test_03():
-    validate(FAIL_TRANSLATOR_1, {"openapi": openapis["openapi_v3"]})
+    validate(FAIL_TRANSLATOR_1, {"openapi": openapis["openapi_v3.0"]})
     with pytest.raises(ValueError):
         validate(FAIL_TRANSLATOR_1, openapis)
 
 
 def test_04():
-    validate(FAIL_TRANSLATOR_2, {"openapi": openapis["openapi_v3"]})
+    validate(FAIL_TRANSLATOR_2, {"openapi": openapis["openapi_v3.0"]})
     with pytest.raises(ValueError):
         validate(FAIL_TRANSLATOR_2, openapis)
 
@@ -51,3 +57,14 @@ def test_05():
         validate(PASS_SWAGGER, openapis)
     with pytest.raises(ValueError):
         validate(PASS_OPENAPI, swaggers)
+
+def test_06():
+    validate(PASS_OPENAPI31, {"openapi": openapis["openapi_v3.1"]})
+    validate(PASS_OPENAPI31, openapis)
+
+def test_07():
+    with pytest.raises(ValueError):
+        validate(FAIL_OPENAPI31, {"openapi": openapis["openapi_v3.1"]})
+    with pytest.raises(ValueError):
+        validate(FAIL_OPENAPI31, openapis)
+
