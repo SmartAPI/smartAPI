f5d59984de007c3302067a828bb3a319e7947d55
Everaldo <everaldorodrigo@gmail.com>
2024-09-25 14:44:37 -0700

    Test github action.

--- .github/scripts/check_backup.py
+++ .github/scripts/check_backup.py
@@ -1,3 +1,32 @@
+"""
+This script checks if a backup file for the current date exists in a specified S3 bucket.
+If the backup file does not exist, a notification is sent to a Slack channel.
+
+Expected file format in the S3 bucket:
+- The file should be in the folder 'db_backup/' with the following naming pattern:
+  'smartapi_YYYYMMDD.zip', where YYYYMMDD corresponds to the current date.
+
+Required Environment Variables:
+- AWS_ACCESS_KEY_ID: The AWS access key ID to read the AWS s3 bucket.
+- AWS_SECRET_ACCESS_KEY: The AWS secret access key  to read the AWS s3 bucket.
+- BACKUP_BUCKET_NAME: The name of the AWS S3 bucket where backups are stored.
+- S3_FOLDER: The folder path within the S3 bucket where backups are stored (e.g., 'db_backup/').
+- AWS_REGION: The AWS region where the S3 bucket is located.
+- SLACK_CHANNEL: The Slack channel where notifications should be sent (e.g., '#observability-test').
+- SLACK_WEBHOOK_URL: The Slack Webhook URL used to send the notification.
+
+Functionality:
+1. The script uses the AWS SDK (boto3) to check for the existence of the backup file in the specified S3 bucket.
+2. If the file is found, it logs that no action is needed.
+3. If the file is not found, it sends a notification to the configured Slack channel.
+
+Dependencies:
+- boto3: For interacting with AWS S3.
+- requests: For sending HTTP POST requests to Slack.
+
+"""
+
+
 import boto3
 import os
 import requests
