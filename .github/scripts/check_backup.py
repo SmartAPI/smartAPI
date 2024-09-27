60528e9b06cd3bd6c8c8d6535396d5f539fd3e29
Everaldo <everaldorodrigo@gmail.com>
2024-09-27 16:26:48 -0700

    Refactor.

--- .github/scripts/check_backup.py
+++ .github/scripts/check_backup.py
@@ -26,7 +26,6 @@ Dependencies:
 
 """
 
-
 import boto3
 import os
 import requests
@@ -42,27 +41,19 @@ expected_file = f"{os.getenv('S3_FOLDER')}smartapi_{today_date}.zip"
 s3_client = boto3.client("s3", region_name=os.getenv("AWS_REGION"))
 
 # Try to fetch the file metadata
-# try:
-s3_client.head_object(Bucket=os.getenv("BACKUP_BUCKET_NAME"), Key=expected_file)
-# print(f"Backup file {expected_file} exists, no action needed")
-# except s3_client.exceptions.ClientError:
-#     print(f"Backup file {expected_file} does NOT exist.")
-
-# Create the payload for Slack
-slack_data = {
-    "channel": os.getenv("SLACK_CHANNEL"),
-    "username": "SmartAPI",
-    "icon_emoji": ":thumbsdown:",
-    "text": f":alert: The backup file {expected_file} was NOT created today!",
-}
-
 try:
-    response = requests.post(os.getenv("SLACK_WEBHOOK_URL"), json=slack_data, timeout=10)
-    if response.status_code == 200:
-        print(" └─ Slack notification sent successfully.")
-    else:
-        print(f" └─ Failed to send message to Slack: {response.status_code}, {response.text}")
-except requests.exceptions.Timeout:
-    print(" └─ Request timed out to Slack WebHook URL.")
-except requests.exceptions.RequestException as e:
-    print(f" └─ Failed to send Slack notification. Error: {str(e)}")
+    s3_client.head_object(Bucket=os.getenv("BACKUP_BUCKET_NAME"), Key=expected_file)
+    print(f"Backup file {expected_file} exists, no action needed")
+
+except s3_client.exceptions.NoSuchKey:
+    print(f"Backup file {expected_file} does NOT exist.")
+
+    # Create the payload for Slack
+    slack_data = {
+        "channel": os.getenv("SLACK_CHANNEL"),
+        "username": "SmartAPI",
+        "icon_emoji": ":thumbsdown:",
+        "text": f":alert: The backup file {expected_file} was NOT created today!",
+    }
+
+    requests.post(os.getenv("SLACK_WEBHOOK_URL"), json=slack_data, timeout=10)
