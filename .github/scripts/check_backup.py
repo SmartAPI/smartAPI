9459e099d3b2e8a1e521b095a67dcd0bd4d5f8d1
Everaldo <everaldorodrigo@gmail.com>
2024-09-27 17:26:07 -0700

    Improve messages.

--- .github/scripts/check_backup.py
+++ .github/scripts/check_backup.py
@@ -47,29 +47,26 @@ try:
     print(f"Backup file {expected_file} exists, no action needed")
 
 except botocore.exceptions.ClientError as e:
-    if not e.response["Error"]["Code"] == "404":
+    print(f" └─ Backup file {expected_file} does NOT exist.")
+
+    # Create the payload for Slack
+    slack_data = {
+        "channel": os.getenv("SLACK_CHANNEL"),
+        "username": "SmartAPI",
+        "icon_emoji": ":thumbsdown:",
+        "text": f":alert: The backup file {expected_file} was NOT created today!",
+    }
+
+    try:
+        print(" └─ Sending Slack notification.")
+        response = requests.post(os.getenv("SLACK_WEBHOOK_URL"), json=slack_data, timeout=10)
+        if response.status_code == 200:
+            print(" └─ Slack notification sent successfully.")
+        else:
+            print(f" └─ Failed to send message to Slack: {response.status_code}, {response.text}")
+    except requests.exceptions.Timeout:
+        print(" └─ Request timed out to Slack WebHook URL.")
         raise e
-    else:
-        print(f" └─ Backup file {expected_file} does NOT exist.")
-
-        # Create the payload for Slack
-        slack_data = {
-            "channel": os.getenv("SLACK_CHANNEL"),
-            "username": "SmartAPI",
-            "icon_emoji": ":thumbsdown:",
-            "text": f":alert: The backup file {expected_file} was NOT created today!",
-        }
-
-        try:
-            print(" └─ Sending Slack notification.")
-            response = requests.post(os.getenv("SLACK_WEBHOOK_URL"), json=slack_data, timeout=10)
-            if response.status_code == 200:
-                print(" └─ Slack notification sent successfully.")
-            else:
-                print(f" └─ Failed to send message to Slack: {response.status_code}, {response.text}")
-        except requests.exceptions.Timeout:
-            print(" └─ Request timed out to Slack WebHook URL.")
-            raise e
-        except requests.exceptions.RequestException as e:
-            print(f" └─ Failed to send Slack notification. Error: {str(e)}")
-            raise e
+    except requests.exceptions.RequestException as e:
+        print(f" └─ Failed to send Slack notification. Error: {str(e)}")
+        raise e
