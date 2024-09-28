7bfffd602090ae199c4f041059e6e3aec60dee51
Everaldo <everaldorodrigo@gmail.com>
2024-09-27 17:23:17 -0700

    Improve messages.

--- .github/scripts/check_backup.py
+++ .github/scripts/check_backup.py
@@ -27,6 +27,7 @@ Dependencies:
 """
 
 import boto3
+import botocore
 import os
 import requests
 
@@ -45,15 +46,30 @@ try:
     s3_client.head_object(Bucket=os.getenv("BACKUP_BUCKET_NAME"), Key=expected_file)
     print(f"Backup file {expected_file} exists, no action needed")
 
-except s3_client.exceptions.NoSuchKey:
-    print(f"Backup file {expected_file} does NOT exist.")
+except botocore.exceptions.ClientError as e:
+    if not e.response["Error"]["Code"] == "404":
+        raise e
+    else:
+        print(f" └─ Backup file {expected_file} does NOT exist.")
 
-    # Create the payload for Slack
-    slack_data = {
-        "channel": os.getenv("SLACK_CHANNEL"),
-        "username": "SmartAPI",
-        "icon_emoji": ":thumbsdown:",
-        "text": f":alert: The backup file {expected_file} was NOT created today!",
-    }
+        # Create the payload for Slack
+        slack_data = {
+            "channel": os.getenv("SLACK_CHANNEL"),
+            "username": "SmartAPI",
+            "icon_emoji": ":thumbsdown:",
+            "text": f":alert: The backup file {expected_file} was NOT created today!",
+        }
 
-    requests.post(os.getenv("SLACK_WEBHOOK_URL"), json=slack_data, timeout=10)
+        try:
+            print(" └─ Sending Slack notification.")
+            response = requests.post(os.getenv("SLACK_WEBHOOK_URL"), json=slack_data, timeout=10)
+            if response.status_code == 200:
+                print(" └─ Slack notification sent successfully.")
+            else:
+                print(f" └─ Failed to send message to Slack: {response.status_code}, {response.text}")
+        except requests.exceptions.Timeout:
+            print(" └─ Request timed out to Slack WebHook URL.")
+            raise e
+        except requests.exceptions.RequestException as e:
+            print(f" └─ Failed to send Slack notification. Error: {str(e)}")
+            raise e
