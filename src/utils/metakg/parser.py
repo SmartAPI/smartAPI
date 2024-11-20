f52acb6ee870d4814b62b3ea4808b026c5ccfab6
Nichollette <nichollette.aco@gmail.com>
2024-11-20 16:36:17 -0500

    added url parameter for parser methods

--- src/utils/metakg/parser.py
+++ src/utils/metakg/parser.py
@@ -13,17 +13,28 @@ class MetaKGParser:
     get_url_timeout = 60
     metakg_errors = None
 
-    def get_non_TRAPI_metadatas(self, data, extra_data=None):
-        parser = API(data)
+    def get_non_TRAPI_metadatas(self, data=None, extra_data=None, url=None): # *** TEST THIS FOR BREAK POINTS ***
+        if data:
+            parser = API(smartapi_doc=data)
+        elif url:
+            parser = API(url=url)
+        else: 
+            return [] # **** ERROR HANDLE THIS ****
         mkg = self.extract_metakgedges(parser.metadata["operations"], extra_data=extra_data)
         no_nodes = len({x["subject"] for x in mkg} | {x["object"] for x in mkg})
         no_edges = len({x["predicate"] for x in mkg})
         logger.info("Done [%s nodes, %s edges]", no_nodes, no_edges)
         return mkg
 
-    def get_TRAPI_metadatas(self, data, extra_data=None):
+    def get_TRAPI_metadatas(self, data=None, extra_data=None, url=None):
         ops = []
-        metadata_list = self.get_TRAPI_with_metakg_endpoint(data)
+        if data:
+            metadata_list = self.get_TRAPI_with_metakg_endpoint(data=data)
+        elif url:
+            metadata_list = self.get_TRAPI_with_metakg_endpoint(url=url)
+        else:
+            return [] # **** ERROR HANDLE THIS ****
+
         count_metadata_list = len(metadata_list)
         self.metakg_errors = {}
         for i, metadata in enumerate(metadata_list):
@@ -34,7 +45,13 @@ class MetaKGParser:
 
         return self.extract_metakgedges(ops, extra_data=extra_data)
 
-    def get_TRAPI_with_metakg_endpoint(self, data):
+    def get_TRAPI_with_metakg_endpoint(self, data=None, url=None): # TEST THIS FOR BREAK POINTS
+        # Use the URL if provided, otherwise fall back to the 'data' argument
+        if data:
+            parser = API(data)
+        elif url:
+            parser = API(url=url)
+
         metadatas = []
         parser = API(data)
         metadata = parser.metadata
